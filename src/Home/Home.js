d567421f3694bfa3840c0e30d3f4b279a03f45c1
Sai <sai081118@gmail.comgit commit>
2021-11-28 18:56:24 +0630

    Production Version

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,26 +1,12 @@
-import { 
-    Container,
-    makeStyles,
-   
-    } from '@material-ui/core'
+
 import React from 'react'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { FetchAysncAnime, FetchAysncMovie, FetchAysncShow} from '../Redux/movieSlice';
-
-
-
 const Home = () => {
     // Fetching movies
   
 
 // MakeStyle from material-UI
-const useStyle =  makeStyles(()=>{
-    return({
-    
-    })
-})
-const classes = useStyle();
+
+
 // React-component
     return (
         <>
